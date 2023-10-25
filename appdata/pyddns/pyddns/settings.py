66a1b9843e6467cedf3881f8944194d3a5fbce57
clutch2sft <148905f4@opayq.com>
2023-10-25 11:59:43 -0400

    Moved secret key out of code

--- appdata/pyddns/pyddns/settings.py
+++ appdata/pyddns/pyddns/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w_mgv7app6__864!-x$zv$*5r(p7@@i*i^tm6c0oou0iw+*lj9'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
